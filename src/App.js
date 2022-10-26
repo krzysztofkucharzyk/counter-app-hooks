56481d0f884355e7d5e4ecc90dce6d4702152a85
krzysztofkucharzyk <krzysztofkucharzyk@gmail.com>
2022-10-26 15:26:22 +0200

    ss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  const inc = () => {
+    setCount(prevCount => prevCount + 1);
+  }
+
+  const dec = () => {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  const stepChange = (event) => {
+    setStep(event.target.value);
+  }
+
+  const add = () => {
+    setSum(prevCount => prevCount += +step);
+  }
+
+  // const changeValue = (action) => {
+  //   setSum((prevCount) => {
+  //     if(action === 'add') {
+  //       prevCount += +step;
+  //     } else {
+  //       prevCount = sum;
+  //     }
+  //   }
+  // )}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className={count > 0 ? 'plus' : count < 0 ? 'minus' : null}>{count}</h1>
+        <div className="counter-panel">
+          <button onClick={dec}>-</button>
+          <button onClick={inc}>+</button>
+        </div>
+        <div className="step">
+          <h1 className={sum > 0 ? 'plus' : sum < 0 ? 'minus' : null}>{sum}</h1>
+          {/* STEP: <input type="number" value={step} onChange={event => setStep(event.target.value)}/> */}
+          STEP: <input type="number" value={step} onChange={stepChange}/>
+          <button onClick={add}>Add {step}</button>
+          {/* <button onClick={() => changeValue('add')}>Add {step}</button>
+          <button onClick={() => changeValue('init')}>Initial value</button> */}
+        </div>
     </div>
   );
 }
